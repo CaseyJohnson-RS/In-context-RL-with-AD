a3a352edcdeade5b96bdfec0f6d74f3ce0237a90
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2025-10-26 13:32:25 +0700

    fix: add karmedbandit_run_in_context to utils package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,10 @@
-from .k_armed_bandit import karmedbandit_generate_traces, karmedbandit_trajectories_to_sequences
+from .k_armed_bandit import karmedbandit_generate_traces, karmedbandit_trajectories_to_sequences, karmedbandit_run_in_context
 from .common import set_seed, batch_iterator
 
 __all__ = [
     "karmedbandit_generate_traces",
     "karmedbandit_trajectories_to_sequences",
+    "karmedbandit_run_in_context",
     "set_seed",
     "batch_iterator"
 ]
