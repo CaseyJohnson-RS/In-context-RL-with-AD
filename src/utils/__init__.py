dde1689430c7b00df1b29cd232ac49f5301eedb6
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2025-10-26 13:09:08 +0700

    fix: add batch_iterator to utils package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,9 @@
 from .k_armed_bandit import generate_karmedbandit_traces, karmedbandit_trajectories_to_sequences
-from .common import set_seed
+from .common import set_seed, batch_iterator
 
 __all__ = [
     "generate_karmedbandit_traces",
     "karmedbandit_trajectories_to_sequences",
     "set_seed",
+    "batch_iterator"
 ]
